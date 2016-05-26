a177b361ec667f6c1ae44b75083c3c0823a71e55
davdkm <davd.km@gmail.com>
2016-05-25 19:06:11 -0700

    Done.

--- code.js
+++ code.js
@@ -1,1 +1,17 @@
 'use strict';
+
+function favIceCream() {
+  return 'I love mint chocolate chip'
+}
+
+function shouting() {
+  return 'I LOVE JAVASCRIPT SOOOOOO MUCH'
+}
+
+function roundDown(number) {
+  return Math.floor(number)
+}
+
+function theTruth() {
+  return true
+}
